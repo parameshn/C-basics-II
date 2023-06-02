ee3c5c73f8e0ea8dcc2c5bc3643c26043b4f604c
Paramesh <parameshdarshan1234@gmail.com>
2023-06-02 20:28:35 +0530

    chapter 9

--- basicoperators.c
+++ basicoperators.c
@@ -159,4 +159,31 @@ around expressions.
     int a = b = c = d = e = f = g = h = i = j = 9; // this is easier
 
     // Because of the right-to-left associativity, C first assigns the 9 to j, then puts the 9 in i, and so on.
+    a = 5 * (b = 2); /* Puts a 2 in b and a 10 in a */
+
+    /* This program calculates the average of four grades and also does
+some other basic math. */
+
+    int grade1, grade2, grade3, grade4;
+    float averageGrade, gradeDelta, percentDiff;
+    /* The student got 88s on the first and third test,
+    so a multiple assignment statement works. */
+    grade1 = grade3 = 88;
+    grade2 = 79;
+    // The user needs to enter the fourth grade
+    printf("What did you get on the fourth test");
+    printf(" (An integer between 0 and 100)?");
+    scanf(" %d", &grade4);
+
+    averageGrade = (grade1 + grade2 + grade3 + grade4) / 4;
+    printf("Your average is %.1f.\n", averageGrade);
+
+    gradeDelta = 95 - averageGrade;
+    percentDiff = 100 * ((95 - averageGrade) / 95);
+
+    printf("Your grade is %.1f points lower than the ", gradeDelta);
+    printf("top grade in the class (95)\n");
+    printf("You are %.1f percent behind ", percentDiff);
+    printf("that grade!\n\n\n");
+    return 0;
 }
