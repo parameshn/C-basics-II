439809399875c8832277c7818100dce3474986e9
Paramesh <parameshdarshan1234@gmail.com>
2023-06-03 21:22:14 +0530

    chapter 12

---
+++ logicalOperators.c
@@ -0,0 +1,16 @@
+////LOGICAL OPERATORS  LEFT TO RIGHT
+/*A relational operator simply tests how two values relate (how they compare to each
+other). The logical operators combine relational operators.*/
+
+/*
+&& AND OPERATOR
+|| OR OPERATOR
+!  NOT OPERATOR
+
+*/
+
+#include <stdio.h>
+
+void main()
+{
+}
